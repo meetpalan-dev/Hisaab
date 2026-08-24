e81e631844915e4cdc833003741b613cf0a619fa
meetpalan-dev <meetpalan.work@email.com>
2026-08-24 06:15:21 +0530

    Update Hisaab

--- app/src/main/java/com/palan/hisaab/ui/addtransaction/AddEditTransactionDialog.kt
+++ app/src/main/java/com/palan/hisaab/ui/addtransaction/AddEditTransactionDialog.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -40,6 +41,7 @@ val DEFAULT_CATEGORIES = listOf(
     "Payment", "Recharge", "Insurance", "Travel", "Shopping", "Bills", "Food", "Other"
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddEditTransactionDialog(
     existing: Transaction?,
